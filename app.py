ab36e3788a8de4b5b4812fadfd3b9998b64456a3
mohitrajdeo <mohitrajdeol6deoghar@gmail.com>
2025-02-14 10:58:30 +0530

    updated path

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ from streamlit_option_menu import option_menu
 #parkinson_model = joblib.load("C:/Users/mohit/OneDrive/Desktop/streamlit/Multiple Disease Prediction/parkinson disease/parkinson_model.pkl")
 #heart_model = joblib.load("C:/Users/mohit/OneDrive/Desktop/streamlit/Multiple Disease Prediction/heart disease/heart_disease_model.pkl")
 
-diabetes_model = pickle.load(open('C:/Users/mohit/OneDrive/Desktop/streamlit/Multiple Disease Prediction/diabetes/diabetes_model.sav','rb'))
-parkinson_model = pickle.load(open('C:/Users/mohit/OneDrive/Desktop/streamlit/Multiple Disease Prediction/parkinson disease/parkinson_model.sav','rb'))
-heart_model = pickle.load(open('C:/Users/mohit/OneDrive/Desktop/streamlit/Multiple Disease Prediction/heart disease/heart_model.sav','rb'))
+diabetes_model = pickle.load(open('diabetes\diabetes_model.sav','rb'))
+parkinson_model = pickle.load(open('parkinson disease\parkinson_model.sav','rb'))
+heart_model = pickle.load(open('heart disease\heart_model.sav','rb'))
 
-scaler = pickle.load(open( 'C:/Users/mohit/OneDrive/Desktop/streamlit/Multiple Disease Prediction/parkinson disease/scalar.sav' ,'rb'))
+scaler = pickle.load(open( 'parkinson disease\scalar.sav' ,'rb'))
 
 # Sidebar navigation
 with st.sidebar:
